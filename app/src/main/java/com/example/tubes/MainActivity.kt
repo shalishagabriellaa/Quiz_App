f4fdeaa9fc2db85db8bf62599a01a9b7b3790d82
alam <wanguntaralambarus@gmail.com>
2025-10-23 21:02:08 +0700

    Setup Firebase & Integrasi Firestore

--- app/src/main/java/com/example/tubes/MainActivity.kt
+++ app/src/main/java/com/example/tubes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tubes
 
 import android.os.Bundle
+import android.util.Log // Alam -> Untuk Logging
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,13 +17,35 @@ import android.content.Intent
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity // Ini jadi ganti ke jetpack compose?
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore // Alam -> Untuk konek database
 
 class MainActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//        db.collection("users")
+//            .get()
+//            .addOnSuccessListener { result ->
+//                if (result.isEmpty) {
+//                    Toast.makeText(this, "No users found in Firestore", Toast.LENGTH_SHORT).show()
+//                    Log.d("Firestore", "No users found.")
+//                } else {
+//                    for (document in result) {
+//                        val name = document.getString("full_name")
+//                        val email = document.getString("email")
+//                        Log.d("Firestore", "User: $name, Email: $email")
+//                    }
+//                    Toast.makeText(this, "Firestore Loaded!", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//            .addOnFailureListener { e ->
+//                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+//                Log.e("Firestore", "Error getting documents", e)
+//            }
         val btnLogout = findViewById<Button>(R.id.btnLogout)
         btnLogout.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
